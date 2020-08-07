d774bd70e4d91d93945054f8089ce0d54b48476d
MohamedElkamary2 <amo263128@gmail.com>
2020-08-07 23:26:51 +0200

    Updates

---
+++ precache-manifest.39f6235e5e4f74bd7154a5d7ae4a8c89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03a8b7158e6dc9d6b0613f27b3e37abc",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "9445dd89401a4ca6ca04",
+    "url": "/Todolist/static/css/main.609017b1.chunk.css"
+  },
+  {
+    "revision": "759b548cc783d0bbd571",
+    "url": "/Todolist/static/js/2.622bc790.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todolist/static/js/2.622bc790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9445dd89401a4ca6ca04",
+    "url": "/Todolist/static/js/main.0b0d70ff.chunk.js"
+  },
+  {
+    "revision": "77a9432d7baa71f0ef7b",
+    "url": "/Todolist/static/js/runtime-main.ae92cba0.js"
+  }
+]);
